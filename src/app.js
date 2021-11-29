e75085a17f61a129144250d81db607bddec7db37
Marrocco-Simone <marrocco.simone00@gmail.com>
2021-11-29 10:35:32 +0100

    changes for heroku

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const { resourceLimits } = require('worker_threads');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //set views location
 const viewsPath = path.join(__dirname,'../templates/views');
